d6b23c9bf2e886b74c23518e94bd4496b1d7bfa4
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-08-30 11:58:16 +0200

    modify images

---
+++ scripts/imagesDB.js
@@ -0,0 +1,31 @@
+let images = {
+    "House": [
+        {
+            "images": "hogwarts"
+        }
+    ],
+
+    "Staff": [
+        {
+            "images": "dumbledore"
+        }
+    ],
+
+    "Students": [
+        {
+            "images": "harry"
+        }
+    ],
+
+    "Spells": [
+        {
+            "images": "wandMagicSparkles"
+        }
+    ],
+
+    "Characters": [
+        {
+            "images": "lordVoldemort"
+        }
+    ]
+}
