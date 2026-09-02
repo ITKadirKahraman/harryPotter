b27fa4ba157bb15eea666dd30a075193e078f048
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-02 23:51:20 +0200

    modify render

--- scripts/render.js
+++ scripts/render.js
@@ -11,8 +11,10 @@ function renderHeader() {
 function renderMain() {
     let card = document.getElementById('mainContainer');
     card.innerHTML = "";
-    for (let indexImages = 0; indexImages < images.length; indexImages++) {
-        card.innerHTML += getMain(indexImages);
+    let categories = Object.keys(images);
+    for (let indexImages = 0; indexImages < categories.length; indexImages++) {
+        let category = images[categories[indexImages]][0];
+        card.innerHTML += getMain(category);
     }
 }
 
