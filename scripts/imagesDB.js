302abe6b5f2af901846d8005a7611689685356e4
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-02 23:50:34 +0200

    update keys

--- scripts/imagesDB.js
+++ scripts/imagesDB.js
@@ -1,36 +1,36 @@
 let images = {
     "House": [
         {
-            "name": "House",
-            "images": "hogwarts"
+            name: "House",
+            images: "hogwarts"
         }
     ],
 
     "Staff": [
         {
-            "name": "Staff",
-            "images": "dumbledore"
+            name: "Staff",
+            images: "dumbledore"
         }
     ],
 
     "Students": [
         {
-            "name": "Students",
-            "images": "harry"
+            name: "Students",
+            images: "harry"
         }
     ],
 
     "Spells": [
         {
-            "name": "Spells",
-            "images": "wandMagicSparkles"
+            name: "Spells",
+            images: "wandMagicSparkles"
         }
     ],
 
     "Characters": [
         {
-            "name": "Characters",
-            "images": "lordVoldemort"
+            name: "Characters",
+            images: "lordVoldemort"
         }
     ]
 }
