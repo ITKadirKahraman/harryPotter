9e5fb8e0741397fc048b41138fac4d2601f7e475
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-04 12:33:24 +0200

    updated render

---
+++ js/render.js
@@ -0,0 +1,28 @@
+function renderPage() {
+    renderHeader();
+    renderMain();
+    renderDialog();
+    renderFooter();
+}
+
+function renderHeader() {
+    return document.getElementById('headerContainer').innerHTML = getHeader();
+}
+
+function renderMain() {
+    let card = document.getElementById('mainContainer');
+    card.innerHTML = "";
+    let categories = Object.keys(images);
+    for (let indexImages = 0; indexImages < categories.length; indexImages++) {
+        let category = images[categories[indexImages]][0];
+        card.innerHTML += getMain(category);
+    }
+}
+
+function renderDialog() {
+    return document.getElementById('dialogContainer').innerHTML = getDialogWithSmallCards();
+}
+
+function renderFooter() {
+    return document.getElementById('footerContainer').innerHTML = getFooter();
+}
