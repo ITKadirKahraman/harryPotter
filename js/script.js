686c5afb106144e4fe9d8055d3b71e3921fb150c
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-04 12:33:51 +0200

    added arrays

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let allObjects = []; // alle geladene Objekte vom API
+let allStudents = []; // alle geladene Studenten vom API
+let allStaffs = []; // alle geladene Lehrkräfte vom API
+let favoritePerons = []; // die favoriten Persone werden gespeichert
+
+function init() {
+    renderPage();
+}
+
