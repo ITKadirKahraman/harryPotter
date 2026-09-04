f1bd09e2dabf4a64ca7cc7403d61299f49dacef5
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-04 12:32:34 +0200

    images

---
+++ js/imagesDB.js
@@ -0,0 +1,36 @@
+let images = {
+    "House": [
+        {
+            name: "House",
+            images: "hogwarts"
+        }
+    ],
+
+    "Staff": [
+        {
+            name: "Staff",
+            images: "dumbledore"
+        }
+    ],
+
+    "Students": [
+        {
+            name: "Students",
+            images: "harry"
+        }
+    ],
+
+    "Spells": [
+        {
+            name: "Spells",
+            images: "wandMagicSparkles"
+        }
+    ],
+
+    "Characters": [
+        {
+            name: "Characters",
+            images: "lordVoldemort"
+        }
+    ]
+}
