a1b7f5d3f1b1152f7e876e40c44c3a4817ef7686
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-05 23:23:42 +0200

    added new functions

--- js/script.js
+++ js/script.js
@@ -7,3 +7,65 @@ function init() {
     renderPage();
 }
 
+async function openCardsCharacters() {
+    
+}
+
+async function openCardsStudents() {
+    
+}
+
+async function openCardsStaff() {
+    
+}
+
+async function openCardsSpells() {
+    
+}
+
+async function openCardsHouseGryffindor() {
+    
+}
+
+async function openCardsHouseSlytherin() {
+    
+}
+
+async function openCardsHouseRavenclaw() {
+    
+}
+
+async function openCardsHouseHufflepuff() {
+    
+}
+
+async function loadCards(characters) {
+    const speciesResponse = await fetch(pokemon.species.url);
+    const speciesData = await speciesResponse.json();
+
+    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionResponse.json();
+
+    const names = getEvolutionNames(evolutionData.chain);
+
+    const container =
+        document.getElementById(`evolutionChain${pokemon.id}`);
+
+    container.innerHTML = "";
+
+    for (const name of names) {
+
+        const response = await fetch(BASE_URL + name);
+
+        const evolutionPokemon = await response.json();
+
+        container.innerHTML += evolutionChain(evolutionPokemon);
+    }
+}
+
+async function renderDialog() {
+    const dialog = document.getElementById('dialog');
+    const pokemon = allPokemons[currentIndex];
+    dialog.innerHTML = getDialogPokemonCard(pokemon);
+    await loadEvolutionChain(pokemon);
+}
