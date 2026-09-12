e8831ee9b4090b5f7d50d6a899db81394723f495
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-12 23:50:45 +0200

    modify house

---
+++ js/houseDB.js
@@ -0,0 +1,29 @@
+let house = {
+    "Gryffindor": [
+        {
+            name: "Gryffindor",
+            images: "gryffindor"
+        }
+    ],
+
+    "Hufflepuff": [
+        {
+            name: "Hufflepuff",
+            images: "hufflepuff"
+        }
+    ],
+
+    "Ravenclaw": [
+        {
+            name: "Ravenclaw",
+            images: "ravenclaw"
+        }
+    ],
+
+    "Slytherin": [
+        {
+            name: "Slytherin",
+            images: "slytherin"
+        }
+    ],
+}
