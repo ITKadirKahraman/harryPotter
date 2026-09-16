fcd906e559b015d5b7216f7b14a868bed28063a3
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-09-16 15:06:49 +0200

    updated a loading screen

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,6 @@
 function showLoading() {
     const loadingScreen = document.getElementById('loadingScreen');
     loadingScreen.style.display = 'flex';
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 3000);
 }
 
 function hideLoading() {
